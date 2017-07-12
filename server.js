a5852f28994106775813e107f231ddd57a945940
Stuart Terrell <sterrell6@cox.net>
2017-07-12 05:34:44 -0700

    Added a project Page

--- server.js
+++ server.js
@@ -43,6 +43,11 @@ app.get('/about', (req, res) => {
         pageTitle: 'About Page'
     });
 });
+app.get('/project', (req, res) => {
+    res.render('project.hbs', {
+        pageTitle: 'Project Page'
+    });
+});
 
 // /bad - send back json with errorMessage
 app.get('/bad', (req, res) => {
